1a4fb1ea69b14ca75cf84e49d5e9546328dbc09c
hajipour <hoseinhajepor@gmail.com>
2022-04-12 17:50:57 +0430

    add package

--- app/Components/Pages/Dashboard/Shop.php
+++ app/Components/Pages/Dashboard/Shop.php
@@ -3,8 +3,10 @@
 namespace App\Components\Pages\Dashboard;
 
 use App\Models\Package;
+use App\Models\Payment;
 use Illuminate\Support\Facades\Route;
 use Livewire\Component;
+use nusoap_client;
 
 class Shop extends Component
 {
@@ -13,13 +15,60 @@ class Shop extends Component
     public function route()
     {
         return Route::get('/shop')
-            ->name('shop');
-        // ->middleware('auth');
+            ->name('shop')
+            ->middleware('auth');
     }
 
     public function render()
     {
         $this->Packages = Package::all();
         return view('pages.dashboard.shop');
+    }
+    public function Buy($package_id){
+
+        $Package = Package::where('id', $package_id)->first();
+        $user = auth()->user();
+        $MerchantID = setting('payment.zarinpal_merchent_code');
+        $zarinpalSandbox = setting('payment.zarinpal_sandbox');
+
+        if ($zarinpalSandbox) {
+            $client = new nusoap_client('https://sandbox.zarinpal.com/pg/services/WebGate/wsdl', 'wsdl');
+        } else {
+            $client = new nusoap_client('https://www.zarinpal.com/pg/services/WebGate/wsdl', 'wsdl');
+        }
+
+
+        $newPay = Payment::create();
+        $newPay->user_id = $user->id;
+        $newPay->package_id = $Package->id;
+        $newPay->amount = $Package->price;
+        $newPay->status = "pending";
+        $newPay->save();
+        $CallbackURL = url('/order?payment_id=' . $newPay->id); // Required
+
+        $client->soap_defencoding = 'UTF-8';
+        $RequestData = [
+            'MerchantID' => $MerchantID,
+            'Amount' => $Package->price,
+            'Description' => $Package->description,
+            'Email' => $user->email,
+            'Mobile' => $user->phone,
+            'CallbackURL' => $CallbackURL,
+        ];
+        $result = $client->call('PaymentRequest', [$RequestData]);
+
+        //Redirect to URL You can do it also by creating a form
+        if ($result['Status'] == 100) {
+            $newPay->authority = $result['Authority'];
+            $newPay->save();
+            if ($zarinpalSandbox) {
+                $result['url'] = 'https://sandbox.zarinpal.com/pg/StartPay/' . $result['Authority'];
+            } else {
+                $result['url'] = 'https://www.zarinpal.com/pg/StartPay/' . $result['Authority'];
+            }
+            return $this->redirect($result);
+        } else {
+            return $this->redirect($result);
+        }
     }
 }
