02c42740f35c6c5afac51d7608300f2f2c8a2ada
hajipour <hoseinhajepor@gmail.com>
2022-04-28 15:20:02 +0430

    bug fix

--- app/Components/Pages/Search/Userlist.php
+++ app/Components/Pages/Search/Userlist.php
@@ -99,9 +99,49 @@ class Userlist extends Component
             $msg .= "شما را به مسابقه دعوت کرده است";
             $this->sendWebNotification(setting('site.title'), $msg, $otherUser->token);
         }
-        
+
         return redirect()->to('/tournament/ready-for-play/' . $TournamentBoard->id);
     }
 
+    public function sendWebNotification($title, $message, $notification_id)
+    {
 
+        $url = 'https://fcm.googleapis.com/fcm/send';
+        $FcmToken = [$notification_id];
+        $serverKey = setting('firebase.token');
+        $data = [
+            "registration_ids" => $FcmToken,
+            "notification" => [
+                "title" => $title,
+                "body" => $message,
+            ]
+        ];
+        $encodedData = json_encode($data);
+        $headers = [
+            'Authorization:key=' . $serverKey,
+            'Content-Type: application/json',
+        ];
+
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+        // Disabling SSL Certificate support temporarly
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $encodedData);
+
+        // Execute post
+        $result = curl_exec($ch);
+
+        if ($result === FALSE) {
+            die('Curl failed: ' . curl_error($ch));
+        }
+
+        // Close connection
+        curl_close($ch);
+    }
 }
